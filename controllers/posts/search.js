b9e5fd84d92aad5bf120d158d34032f85e0981b5
chandlerenterprises <ethan.chandler@metmail.org>
2016-12-06 20:11:34 +0000

    s

--- controllers/posts/search.js
+++ controllers/posts/search.js
@@ -1,7 +1,51 @@
+var exists = require(global.base+'/middleware/exists')
 
 module.exports = function(req, res) {
   
   var regex = {'$regex': /^.*\b(req.body.keyword)\b.*$/};
+  var data = req.body.data;
+  var titles = [];
+  var blogPosts;
+
+  if(data.category) {
+    if(exists(res, data, 'category')) {
+      var category = global.blogPosts[data.category];
+      titles = Object.keys(category);
+      if(!titles[0]) return res.error('category empty');
+      blogPosts = category;
+    }
+  } else {
+    blogPosts = global.blogPosts;
+    var categories = Object.keys(blogPosts);
+    if(!categories[0]) return res.error('no categories');
+    for(var i in categories) {
+      titles.concat(Object.keys(blogPosts[category[i]]));
+    } 
+  }
+  
+  var matches = {};
+  
+  for(var i in titles) {
+    var post = blogPosts[titles[i]];
+    var postKeys = Object.keys(post);
+    
+    for(var a in postKeys) {
+      if(data.keyword && post[postKeys[a]].indexOf(data.keyword) >= 0) {
+        var currentMatchedTitles = Object.keys(matches)
+        
+        for(var b in currentMatchedTitles) {
+          if(currentMatchedTitles[b] == titles[i]) {
+            continue;
+          }
+        }
+        
+        matches[titles[i]] = post;
+        
+      }
+      
+    }
+
+  }
   
   var searchQuery = {
     sort: 'ascending/descending',
